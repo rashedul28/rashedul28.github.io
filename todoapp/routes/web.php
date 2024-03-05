382dd626da22ad30561cacd9f8bb0924aa05324f
rashedul28 <rashedulislamridoy@gmail.com>
2024-03-06 00:16:27 +0600

    dockher use on todo app

---
+++ todoapp/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\TodoController;
+use App\Models\Todo;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [TodoController::class, 'index']);
+
+Route::get('create', [TodoController::class, 'create']);
+Route::post('store-data', [TodoController::class, 'store']);
+
+Route::get('details/{todo}', [TodoController::class, 'details']);
+Route::get('edit/{todo}', [TodoController::class, 'edit']);
+Route::post('update/{todo}', [TodoController::class, 'update']);
+
+Route::get('delete/{todo}', [TodoController::class, 'delete']);
